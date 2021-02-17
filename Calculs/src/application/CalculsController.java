c67c3522b9808366345094afe7ac3b1dacbc0175
amandatishler <amandatishler@amandas-mbp.lan>
2021-02-17 17:35:59 -0500

    calculs

--- Calculs/src/application/CalculsController.java
+++ Calculs/src/application/CalculsController.java
@@ -1,11 +1,15 @@
 package application;
 
+import java.text.DecimalFormat;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
 
 public class CalculsController {
 
@@ -36,8 +40,10 @@ public class CalculsController {
     {
     	try 
     	{	
-			int nbr = Integer.parseInt(a.getText());
-		} catch (NumberFormatException e) {
+			Integer.parseInt(a.getText());
+		} 
+    	catch (NumberFormatException e) 
+    	{
 			Alert alert = new Alert(AlertType.ERROR);
 			alert.setHeaderText("Attention - Erreur");
 			alert.setTitle("Erreur");
@@ -47,4 +53,36 @@ public class CalculsController {
 		}
     }
 
+   @FXML
+   void verifText(KeyEvent e)
+   {
+	   TextField txt = (TextField)e.getSource();
+	   checkNum(txt);
+   }
+    
+   @FXML
+   void calculs(ActionEvent e)
+   {
+	 double num1 = Double.parseDouble(txt1.getText());
+	 double num2 = Double.parseDouble(txt2.getText()); 
+	 double resultat = 0;		  
+	 Button btn = (Button)e.getSource();
+	 
+	 if(btn.getId().equals("add"))
+	 	resultat = num1 + num2;
+	 else
+		 if(btn.getId().equals("sous"))
+			 resultat = num1 - num2;
+		 else
+			 if(btn.getId().equals("mult"))
+				 resultat = num1 * num2;
+			 else
+				 if(btn.getId().equals("divi"))
+					 resultat = num1 / num2;
+	
+	 DecimalFormat nformat = new DecimalFormat("#0.00");
+	 res.setText(nformat.format(resultat));
+	 
+   }   
+   
 }
